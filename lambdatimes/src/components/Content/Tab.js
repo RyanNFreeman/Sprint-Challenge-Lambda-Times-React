94fb4e0551365a7576326f101be8d5df34a57b52
Ryan Freeman <ryanfreeman@Ryans-iMac.local>
2019-02-22 09:44:19 -0700

    working on Tab.js and getting an error. Commented out some code so that I could commit a working app.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+      let tab = '';
+
+      if (props.tab === props.selectedTab) {
+        tab = 'tab active-tab';
+      }
+
+      else {
+        tab = 'tab';
+      }
+
+      tab = (props.tab)
+    
   return (
     <div
-      className={''}
+      className={tab}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         props.selectTabHandler(props.tab)
       }}
     >
-      {props.tab.toUpperCase()}
+      {/* {props.tab.toUpperCase()} */}
     </div>
   );
 };
 
 // Make sure you include PropTypes on your props.
+
+// tab.propTypes 
 
 export default Tab;
